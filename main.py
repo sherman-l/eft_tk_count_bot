ee360b0ef6102c80cf7cae097dbbd3ee004d5f9b
sherman-l <43489501+sherman-l@users.noreply.github.com>
2021-07-12 20:14:28 -0700

    Implemented $stats function to show current number of tks for a user. Added string literal definitions for generic messages/error messages.

--- main.py
+++ main.py
@@ -1,11 +1,29 @@
 from kill_history import Kill_history
 from kill_record import Kill_record
+from dotenv import load_dotenv
 import os
 import json
 import discord
 
 client = discord.Client()
 db_name = "kill_db.json"
+load_dotenv()
+
+### Error Strings ###
+tk_invalid_params = 'Invalid number of params! Appropriate use: ```\'$tk @killer @killed\'```'
+stats_invalid_params = 'Incorrect usage! ```\'$stats @user\'```'
+
+### General Message Strings ###
+no_statistics_user = 'No stats available for {}'
+no_statistics_server = 'No stats available for server'
+kill_stats = 'User: {} \nTeam Kills: {}'
+
+### Keys ###
+player_kill_stats = "player_kill_stats"
+kill_count = "kill_count"
+kill_log = "kill_log"
+user_name = "user_name"
+
 
 def mention_user(user_id):
   return "<@!" + user_id + ">"
@@ -24,8 +42,21 @@ def write_db_json(json_obj):
   json.dump(json_obj, kill_db, indent=4)
   kill_db.close()
 
+async def handle_stats(message):
+  server = str(message.guild.id)
+  user = str(message.mentions[0].id)
+  json_obj = load_db_json()
+  if server in json_obj:
+    if user in json_obj[server][player_kill_stats]:
+      result_message = kill_stats.format(mention_user(user), json_obj[server][player_kill_stats][user][kill_count])
+      await message.channel.send(result_message)
+    else:
+      await message.channel.send(no_statistics_user.format(mention_user(user)))
+  else: 
+    await message.channel.send(no_statistics_server)
+
 async def handle_tk(message):
-  server = message.guild.id
+  server = str(message.guild.id)
   killer = message.mentions[0]
   killed = message.mentions[1]
   tk_history = Kill_history(killer.id, killed.id, message.created_at)
@@ -33,19 +64,19 @@ async def handle_tk(message):
   
   if server not in json_obj:
     json_obj[server] = {}
-    json_obj[server]["player_kill_stats"] = {}
-    json_obj[server]["kill_log"] = []
+    json_obj[server][player_kill_stats] = {}
+    json_obj[server][kill_log] = []
     
-  if tk_history.killer_id in json_obj[server]["player_kill_stats"]:
-    json_obj[server]["player_kill_stats"][tk_history.killer_id]["kill_count"] += 1
+  if tk_history.killer_id in json_obj[server][player_kill_stats]:
+    json_obj[server][player_kill_stats][tk_history.killer_id][kill_count] += 1
   else:
-    json_obj[server]["player_kill_stats"][tk_history.killer_id] = {}
-    json_obj[server]["player_kill_stats"][tk_history.killer_id]["kill_count"] = 1
-    json_obj[server]["player_kill_stats"][tk_history.killer_id]["user_name"] = killer.name
+    json_obj[server][player_kill_stats][tk_history.killer_id] = {}
+    json_obj[server][player_kill_stats][tk_history.killer_id][kill_count] = 1
+    json_obj[server][player_kill_stats][tk_history.killer_id][user_name] = killer.name
     await message.channel.send(mention_user(tk_history.killer_id) + " team killing " + mention_user(tk_history.killed_id) + " log entered!")
 
   kill_log_entry = json.loads(tk_history.to_json_string())
-  json_obj[server]["kill_log"].append(kill_log_entry)
+  json_obj[server][kill_log].append(kill_log_entry)
   print(json_obj)
   write_db_json(json_obj)
 
@@ -62,8 +93,13 @@ async def on_message(message):
   if message.content.startswith('$tk'):
     print(message.mentions)
     if len(message.mentions) != 2:
-      await message.channel.send('Invalid number of params! Appropriate use: ```\'$tk @killer @killed\'```')
+      await message.channel.send(tk_invalid_params)
       return
     await handle_tk(message)
-        
+    
+  if message.content.startswith('$stats'):
+    if len(message.mentions) != 1:
+      await message.channel.send(stats_invalid_params)
+    await handle_stats(message)
+          
 client.run(os.getenv('TOKEN'))
